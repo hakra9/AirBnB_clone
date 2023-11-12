7fd51b649bb92433a2a72fa117997f00068f1c52
Your Name <you@example.com>
2023-11-12 15:39:46 -0800

    h

--- console.py
+++ console.py
@@ -14,25 +14,11 @@ from models.review import Review
 
 
 def parse(arg):
-    curly_braces = re.search(r"\{(.*?)\}", arg)
-    brackets = re.search(r"\[(.*?)\]", arg)
-    if curly_braces is None:
-        if brackets is None:
-            return [i.strip(",") for i in split(arg)]
-        else:
-            lexer = split(arg[:brackets.span()[0]])
-            retl = [i.strip(",") for i in lexer]
-            retl.append(brackets.group())
-            return retl
-    else:
-        lexer = split(arg[:curly_braces.span()[0]])
-        retl = [i.strip(",") for i in lexer]
-        retl.append(curly_braces.group())
-        return retl
-
+    matches = re.findall(r'[{[]([^}\]]+)[}\]]', arg)
+    return [match.strip(",") for match in matches]
 
 class HBNBCommand(cmd.Cmd):
-    """Defines the HolbertonBnB command interpreter.
+    """Defines the  command interpreter.
     Attributes:
         prompt (str): The command prompt.
     """
