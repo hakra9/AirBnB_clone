35cf271ffb9dfca09fe40073d2576251dbb633a1
Your Name <you@example.com>
2023-11-12 15:12:46 -0800

    h

--- console.py
+++ console.py
@@ -2,6 +2,9 @@
 """scripte of HBnB console."""
 import cmd
 import re
+from shlex import split
+from models import storage
+from models.base_model import BaseModel
 
 def simple_parse(arg):
     curly_braces = re.search(r"\{(.*?)\}", arg)
