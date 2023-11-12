e9d22a11c83d39d4a04dfb35fcc8db616eaecd94
Amr Taman <96953623+AmrTaman@users.noreply.github.com>
2023-11-12 23:56:48 +0200

    Update console.py

--- console.py
+++ console.py
@@ -21,14 +21,5 @@ class HBNBCommand(cmd.Cmd):
         """an empty line + ENTER shouldn’t execute anything"""
         pass
 
-    def help_quit(self):
-        """"help message"""
-        print("Quit command to exit the program\n")
-
-    def help_EOF(self):
-        """help message"""
-        print("EOF command to exit the program\n")
-
-
 if __name__ == '__main__':
     HBNBCommand().cmdloop()
