e825360397995b6d64845b0a3d7949abdb199816
Amr Taman <96953623+AmrTaman@users.noreply.github.com>
2023-11-12 21:36:21 +0200

    Update file_storage.py

--- models/engine/file_storage.py
+++ models/engine/file_storage.py
@@ -1,35 +1,37 @@
 #!/user/bin/python3 
-"""MOdule"""
+"""
+Filestorage class that is responsible for serializing and deserializing 
+the jsonfiles 
+
+"""
+
 import json
 from models.base_model import BaseModel
-from models.user import User
-from models.state import State
-from models.city import City
-from models.place import Place
-from models.amenity import Amenity
-from models.review import Review
 
 
-class FileStorage:
-    """serializes ins to a JSON file and deserializes JSON file to ins"""
+class FileStorage():
+
     __file_path = "file.json"
     __objects = {}
 
     def all(self):
-        """Return the dictionary __objects."""
-        return self.__objects
+        """returns the objects dictionary"""
+        return (self.__objects)
 
     def new(self, obj):
-        """Set in __objects obj with key <obj_class_name>.id"""
-        name = obj.__class__.__name__
-        self.__objects["{}.{}".format(name, obj.id)] = obj
+        """adds new objects to the objects dictionar"""
+        if obj:
+            self.__objects["{}.{}".format(
+                obj.__class__.__name__, obj.id)] = obj
 
     def save(self):
-        """serializes __objects to the JSON file """
-        odict = self.__objects
-        objdict = {obj: odict[obj].to_dict() for obj in odict.keys()}
-        with open(self.__file_path, "w") as f:
-            json.dump(objdict, f)
+        """save the objects to a json file"""
+        obj_dict = {}
+
+        for key, value in self.__objects.items():
+            obj_dict[key] = value.to_dict()
+        with open(self.__file_path, 'w') as file:
+            json.dump(obj_dict, file)
 
     def reload(self):
         """reads the objects dictionary in json file then turns it to objects"""
@@ -39,5 +41,7 @@ class FileStorage:
             for key, value in obj_dict.items():
                 obj = BaseModel(**value)
                 self.__objects[key] = obj
+
         except FileNotFoundError:
             pass
+
