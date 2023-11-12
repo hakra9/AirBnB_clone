77f0085f42d0c967a28cf28270c37aaf19d8f8bd
Amr Taman <96953623+AmrTaman@users.noreply.github.com>
2023-11-12 21:31:23 +0200

    Update file_storage.py

--- models/engine/file_storage.py
+++ models/engine/file_storage.py
@@ -1,4 +1,5 @@
 #!/user/bin/python3 
+"""MOdule"""
 import json
 from models.base_model import BaseModel
 from models.user import User
@@ -7,23 +8,29 @@ from models.city import City
 from models.place import Place
 from models.amenity import Amenity
 from models.review import Review
+
+
 class FileStorage:
     """serializes ins to a JSON file and deserializes JSON file to ins"""
     __file_path = "file.json"
     __objects = {}
+
     def all(self):
         """Return the dictionary __objects."""
-        return FileStorage.__objects
+        return self.__objects
+
     def new(self, obj):
         """Set in __objects obj with key <obj_class_name>.id"""
         name = obj.__class__.__name__
-        FileStorage.__objects["{}.{}".format(name, obj.id)] = obj
+        self.__objects["{}.{}".format(name, obj.id)] = obj
+
     def save(self):
         """serializes __objects to the JSON file """
         odict = FileStorage.__objects
         objdict = {obj: odict[obj].to_dict() for obj in odict.keys()}
         with open(FileStorage.__file_path, "w") as f:
             json.dump(objdict, f)
+
     def reload(self):
         """reads the objects dictionary in json file then turns it to objects"""
         try:
