7daaa0eba1152c3b8fedb6f311676332cf04d472
Your Name <you@example.com>
2023-11-12 10:00:54 -0800

    h

---
+++ models/engine/file_storage.py
@@ -0,0 +1,31 @@
+#!/user/bin/python3 
+import json
+from models.base_model import BaseModel
+from models.user import User
+from models.state import State
+from models.city import City
+from models.place import Place
+from models.amenity import Amenity
+from models.review import Review
+
+class FileStorage:
+    """serializes ins to a JSON file and deserializes JSON file to ins"""
+    __file_path = "file.json"
+    __objects = {}
+
+    def all(self):
+        """Return the dictionary __objects."""
+        return FileStorage.__objects
+
+    def new(self, obj):
+        """Set in __objects obj with key <obj_class_name>.id"""
+        name = obj.__class__.__name__
+        FileStorage.__objects["{}.{}".format(name, obj.id)] = obj
+
+    def save(self):
+        """serializes __objects to the JSON file """
+        odict = FileStorage.__objects
+        objdict = {obj: odict[obj].to_dict() for obj in odict.keys()}
+        with open(FileStorage.__file_path, "w") as f:
+            json.dump(objdict, f)
+
