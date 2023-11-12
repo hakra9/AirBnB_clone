776d4fb50892241bcda462db324e1c64261d9d7c
Amr Taman <96953623+AmrTaman@users.noreply.github.com>
2023-11-13 00:03:06 +0200

    Update console.py

--- console.py
+++ console.py
@@ -4,38 +4,31 @@ import cmd
 
 
 class HBNBCommand(cmd.Cmd):
-    """cmd class"""
-    prompt = '(hbnb) '
+    """interactive cmd class"""
+
+    prompt = "(hbnb) "
 
     def do_quit(self, arg):
-        """Quit command to exit the program."""
-        print("Exiting the program.")
+        """Quit the cmd"""
         return True
-
-    def help_quit(self):
-        """Help for the quit command.
-        
-        Usage: quit
-        
-        This command exits the program.
-        """
-        print("Type 'quit' to exit the program.")
 
     def do_EOF(self, arg):
-        """Exit the program on EOF (Ctrl-D)."""
-        print("\nExiting the program.")
+        """Exits the cmd"""
+        print("")
         return True
 
-    def help_EOF(self):
-        """Help for EOF.
-        
-        This command exits the program when Ctrl-D is pressed.
-        """
-        print("Press Ctrl-D to exit the program.")
-
     def emptyline(self):
-        """Do nothing on empty line."""
+        """an empty line + ENTER shouldn’t execute anything"""
         pass
+
+    def help_quit(self):
+        """"help message"""
+        print("Quit command to exit the program\n")
+
+    def help_EOF(self):
+        """help message"""
+        print("EOF command to exit the program\n")
+
 
 if __name__ == '__main__':
     HBNBCommand().cmdloop()
